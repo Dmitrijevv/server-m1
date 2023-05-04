da6086ca0be5257a519a7ec8007c001994436877
Dmitrijevv <88305917+Dmitrijevv@users.noreply.github.com>
2023-05-04 11:47:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./router.js');
+// const fileUpload from 'express-fileupload'
+const cors = require('cors');
+const path = require('path');
+
+var PORT = process.env.PORT || 8000;
+const bd_url = `mongodb+srv://dmitrijevv:qwerty1234@cluster0.1pl8iqe.mongodb.net/test`;
+
+
+const app = express();
+
+app.use(express.json({ extend: true }));
+app.use(cors());
+app.use(express.static('static'));
+// app.use(fileUpload({}));
+app.use('/api', router);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+app.get('/', (req, res) => {
+    res.end('<h1>Welcome</h1>')
+})
+
+async function startApp() {
+    try {
+        await mongoose.connect(bd_url, { useUnifiedTopology: true, useNewUrlParser: true });
+        app.listen(PORT, () => {
+            console.log('server started on port ' + PORT)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startApp()
